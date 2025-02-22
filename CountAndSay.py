e40e5c3cbdf835e1fcc6d59e674f7f1a9271ebc1
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-22 12:00:00 +0530

    Update

--- CountAndSay.py
+++ CountAndSay.py
@@ -1,24 +1,24 @@
-num = 1211
-nextNum = ''
-c=1
-while num>0:
-    l_dig = num%10
-    num = num // 10
-    second_l_dig = num% 10
-    print(f"{l_dig} m {num}, {second_l_dig}")
-    if l_dig == second_l_dig:
-        print("in if")
-        c += 1
-        temp = f"{c}{l_dig}"
-        if num < 9:
-            nextNum = f"{c}{l_dig}" + nextNum
-            break
-    else:
-        print("in else - ", nextNum)
-        nextNum = f"{c}{l_dig}" + nextNum
-        c=1
-    print("Next num : ", nextNum)
-print(nextNum)
+# num = 1211
+# nextNum = ''
+# c=1
+# while num>0:
+#     l_dig = num%10
+#     num = num // 10
+#     second_l_dig = num% 10
+#     print(f"{l_dig} m {num}, {second_l_dig}")
+#     if l_dig == second_l_dig:
+#         print("in if")
+#         c += 1
+#         temp = f"{c}{l_dig}"
+#         if num < 9:
+#             nextNum = f"{c}{l_dig}" + nextNum
+#             break
+#     else:
+#         print("in else - ", nextNum)
+#         nextNum = f"{c}{l_dig}" + nextNum
+#         c=1
+#     print("Next num : ", nextNum)
+# print(nextNum)
 
 
 def printNCountAndSay(n):
