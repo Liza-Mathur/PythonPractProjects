95d3216def905337a4f8cf87204635dd275e94ed
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-25 15:04:04 +0530

    Pascal Triangle:

---
+++ PascalTriangle.py
@@ -0,0 +1,12 @@
+def generate_pascals_triangle(n):
+    triangle = [[1] * (i + 1) for i in range(n)]
+
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+    for row in triangle:
+        print(" ".join(map(str, row)).center(n*3))  # Formatting for alignment
+
+# Example usage
+generate_pascals_triangle(5)
