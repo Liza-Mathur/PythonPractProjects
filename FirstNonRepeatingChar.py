a906844311321be435042b786b640138ab80fe31
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-25 17:15:37 +0530

    First non repeating char in a string

---
+++ FirstNonRepeatingChar.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def first_non_repeating_char(s):
+    char_count = Counter(s)
+
+    for char in s:
+        if char_count[char] == 1:
+            return char
+    return None
+
+print(first_non_repeating_char("aabbcckdeff")) 
