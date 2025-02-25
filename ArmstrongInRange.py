c9669ca903a2a2f69016f73c85096dddc9cf2fa8
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-25 15:11:39 +0530

    Armstrng in range

---
+++ ArmstrongInRange.py
@@ -0,0 +1,10 @@
+def is_armstrong(num):
+    power = len(str(num))
+    return num == sum(int(digit) ** power for digit in str(num))
+
+def armstrong_numbers_in_range(start, end):
+    return [num for num in range(start, end + 1) if is_armstrong(num) and num>9]
+
+# Example usage
+print(armstrong_numbers_in_range(1, 500))  
+# Output: [1, 153, 370, 371, 407]
